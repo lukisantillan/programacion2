1973313b667573a59a3886ee59a4d9210e63bdc3
ggrizlyy <covalautaro@gmail.com>
2024-03-20 17:48:56 -0300

    palindromo docs

--- tp1/funciones.h
+++ tp1/funciones.h
@@ -3,6 +3,12 @@
 #ifndef TP1
 #define TP1
 
+/**
+ * @brief Verifica si una cadena de caracteres es un palíndromo.
+ * 
+ * @param str La cadena de caracteres a verificar.
+ * @return true si la cadena es un palíndromo, false en caso contrario.
+ */
 bool palindromo(char str[]);
 
 #endif
