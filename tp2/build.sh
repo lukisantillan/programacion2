588a0fd6ff494324ae066486875dc5382977978f
ggrizlyy <covalautaro@gmail.com>
2024-04-17 15:21:56 -0300

    Agregado soporte para elegir el tipo de lista en el script build.sh

--- tp2/build.sh
+++ tp2/build.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
 echo "Compilando y ejecutando el programa..."
-gcc -o compiled.out main.c elementos/tipo_elemento.c listas/listas_arreglos.c p2.c p3.c p4.c p5.c p6.c
-./compiled.out
+gcc -o arreglos.out main.c elementos/tipo_elemento.c listas/listas_arreglos.c p2.c p3.c p4.c p5.c p6.c
+gcc -o punteros.out main.c elementos/tipo_elemento.c listas/listas_punteros.c p2.c p3.c p4.c p5.c p6.c
+gcc -o cursores.out main.c elementos/tipo_elemento.c listas/lista_cursores.c p2.c p3.c p4.c p5.c p6.c
 
-echo "Limpiando archivos..."
-rm compiled.out
+# ask for what type of list to use
+clear
+echo "¿Qué tipo de lista desea utilizar?"
+echo "1. Arreglos"
+echo "2. Punteros"
+echo "3. Cursores"
+
+read -p "Ingrese el número de la opción deseada: " option
+
+clear
+case $option in
+    1)
+        ./arreglos.out
+        ;;
+    2)
+        ./punteros.out
+        ;;
+    3)
+        ./cursores.out
+        ;;
+    *)
+        echo "Opción inválida"
+        ;;
+esac
